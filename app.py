ca2c7985791867f228ce1b3217612f6144bbccb0
awestermann96 <89751756+awestermann96@users.noreply.github.com>
2022-12-11 15:05:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 import plotly.express as px
 
 st.title('This is an app')
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
+fig.show()
